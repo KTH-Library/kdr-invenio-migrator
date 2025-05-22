442572294578b20dfcae31eafba2286cdc89a88b
Sam Arbid <samaa2@kth.se>
2025-05-23 00:18:35 +0200

    feat: update community ID and enhance configuration settings

--- invenio_migrator/config.py
+++ invenio_migrator/config.py
@@ -13,9 +13,13 @@ CONFIG = {
     "SOURCE_API_TOKEN": os.getenv("SOURCE_API_TOKEN"),
     # InvenioRDM settings
     "TARGET_BASE_URL": "https://127.0.0.1:5000/api",
+    # "TARGET_BASE_URL": "https://sandbox.datarepository.kth.se/api",
     "TARGET_API_TOKEN": os.getenv("TARGET_API_TOKEN"),
-    # "INVENIORDM_COMMUNITY_ID": "c9caea1c-c355-40d0-b285-9ebc797835ff",
-    "INVENIORDM_COMMUNITY_ID": "kth-community-on-zenodo",
+    # Local KDR community ID
+    # "INVENIORDM_COMMUNITY_ID": "21f6dd7d-f98d-489e-b658-3db9aa459f13",
+    # Sandbox community ID
+    # "INVENIORDM_COMMUNITY_ID": "1ef9e2c5-b11b-448f-985f-1d2e21a42095",
+    "COMMUNITY_REVIEW_CONTENT": "👾👾👾 Auto generated using KDR migration tool 👾👾👾",
     "RATE_LIMITS": {
         "SOURCE_REQUEST_DELAY_SECONDS": 2,
         "INVENIORDM_REQUEST_DELAY_SECONDS": 1,
@@ -28,5 +32,8 @@ CONFIG = {
     "MIGRATION_OPTIONS": {
         "DRY_RUN": False,
         "STOP_ON_ERROR": False,
+    },
+    "SESSION": {
+        "VERIFY_SSL": False,  # Only for testing!
     },
 }
